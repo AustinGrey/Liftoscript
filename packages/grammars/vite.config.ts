e50ad4748b88d31db910051ef8bbb56fced1e3fe
AustinGrey <corey.crapo@gmail.com>
2026-04-24 11:32:26 -0600

    Add an alias for simplifying this migration

--- packages/grammars/vite.config.ts
+++ packages/grammars/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite-plus";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   pack: {
     dts: {
       tsgo: true,
