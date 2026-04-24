67ecc19c4ebfc34c9c14a35f46631377c718586e
AustinGrey <corey.crapo@gmail.com>
2026-04-24 15:38:44 -0600

    Pull the heart of the evaluation out, so I can clean it up and refine.

--- packages/grammars/tests/index.test.ts
+++ packages/grammars/tests/index.test.ts
@@ -2,6 +2,13 @@ import { expect, it } from "vite-plus/test";
 import { parser } from "@/parsers/logic";
 import { LiftoscriptEvaluator } from "@/evaluators/logic-evaluator";
 
+function evalLogic(logic: string) {
+  const parsed = parser.parse(logic);
+  const rawResult = new LiftoscriptEvaluator(script, { foo: 4 }).evaluate(
+    parsed.topNode,
+  );
+}
+
 it("ternary", () => {
   const script = `state.foo > 3 ? state.foo < 7 ? 4 : 5 : 6`;
   const parsed = parser.parse(script);
