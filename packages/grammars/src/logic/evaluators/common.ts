12a46ae5e657064aecbd1a5a3f5d3ea62fa9513d
AustinGrey <corey.crapo@gmail.com>
2026-04-26 21:03:54 -0600

    Try to shore up that typing so it functions as expected.

--- packages/grammars/src/logic/evaluators/common.ts
+++ packages/grammars/src/logic/evaluators/common.ts
@@ -12,34 +12,30 @@ import { is, isBoolean, isNumber } from "@/utils/types.ts";
  * Also maintains the type
  * @param val The value to transform
  * @param handlers The set of mappers for each potential type of value
- * @param defaultValue The default result if an unexpected value is passed, which no handler maps on.
+ * @param defaultValue The default result if an unexpected value is passed, which no handler maps on, or handler was missed but needed.
  *    Defensive programming: This can happen if the types aren't set up water tight and an unexpected type of value is given to the function
  */
 export function transformLogicResult<
   T extends LogicResult,
-  TResultIfNumber extends LogicResult,
-  TResultIfWeight extends LogicResult,
-  TResultIfDynamicWeight extends LogicResult,
-  TResultIfBoolean extends LogicResult,
-  TResultIfUndefined extends LogicResult,
-  TDefault extends LogicResult,
+  TResultIfNumber,
+  TResultIfWeight,
+  TResultIfDynamicWeight,
+  TResultIfBoolean,
+  TResultIfUndefined,
+  TDefault,
 >(
   val: T,
   handlers: (T extends number
     ? { number: (val: number) => TResultIfNumber }
-    : never) &
-    (T extends IWeight
-      ? { weight: (val: IWeight) => TResultIfWeight }
-      : never) &
+    : {}) &
+    (T extends IWeight ? { weight: (val: IWeight) => TResultIfWeight } : {}) &
     (T extends IDynamicWeight
       ? { dynamicWeight: (val: IDynamicWeight) => TResultIfDynamicWeight }
-      : never) &
-    (T extends boolean
-      ? { boolean: (val: boolean) => TResultIfBoolean }
-      : never) &
+      : {}) &
+    (T extends boolean ? { boolean: (val: boolean) => TResultIfBoolean } : {}) &
     (T extends undefined
       ? { undefined: (val: undefined) => TResultIfUndefined }
-      : never),
+      : {}),
   defaultValue: TDefault,
 ):
   | TResultIfNumber
@@ -48,20 +44,31 @@ export function transformLogicResult<
   | TResultIfBoolean
   | TResultIfUndefined
   | TDefault {
-  if (isNumber(val)) {
+  if (isNumber(val) && "number" in handlers) {
     return handlers.number(val);
   }
-  if (is(TWeight, val)) {
+  if (is(TWeight, val) && "weight" in handlers) {
     return handlers.weight(val);
   }
-  if (is(TDynamicWeight, val)) {
+  if (is(TDynamicWeight, val) && "dynamicWeight" in handlers) {
     return handlers.dynamicWeight(val);
   }
-  if (isBoolean(val)) {
+  if (isBoolean(val) && "boolean" in handlers) {
     return handlers.boolean(val);
   }
-  if (val === undefined) {
+  if (val === undefined && "undefined" in handlers) {
     return handlers.undefined(val);
   }
   return defaultValue;
 }
+
+let foo: number | boolean = false as number | boolean;
+
+const bar = transformLogicResult(
+  foo,
+  {
+    number: (x) => x,
+    boolean: (x) => x,
+  },
+  0,
+);
