091abe5764cc1d6d160bf57816a9a5183ad60350
AustinGrey <corey.crapo@gmail.com>
2026-05-18 07:05:43 -0600

    prune

--- packages/grammars/src/utils/math.ts
+++ packages/grammars/src/utils/math.ts
@@ -2,20 +2,12 @@ export function MathUtils_round(value: number, to: number): number {
   return MathUtils_roundFloat(Math.round(value / to) * to, 4);
 }
 
-export function MathUtils_roundTo05(value: number): number {
-  return MathUtils_round(value, 0.5);
-}
-
 export function MathUtils_roundTo005(value: number): number {
   return MathUtils_round(value, 0.05);
 }
 
 export function MathUtils_roundTo0005(value: number): number {
   return MathUtils_round(value, 0.005);
-}
-
-export function MathUtils_roundTo00005(value: number): number {
-  return MathUtils_round(value, 0.0005);
 }
 
 export function MathUtils_roundTo000005(value: number): number {
@@ -68,35 +60,6 @@ export function MathUtils_clamp(
   } else {
     return value;
   }
-}
-
-export function MathUtils_toWord(num?: number): string | undefined {
-  if (num == null) {
-    return undefined;
-  }
-  return [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-    "eleven",
-    "twelve",
-  ][num];
-}
-
-export function MathUtils_parse(value?: string): number | undefined {
-  if (value == null) {
-    return undefined;
-  }
-  const num = Number(value);
-  return isNaN(num) ? undefined : num;
 }
 
 export function n(value: number, precision: number = 2): string {
