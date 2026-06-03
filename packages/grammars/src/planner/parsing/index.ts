354bcf67c46123eac4dc4564e446c10ffc512b2e
AustinGrey <corey.crapo@gmail.com>
2026-06-02 23:30:50 -0600

    Make a parser that returns nodes you can get a source from

--- packages/grammars/src/planner/parsing/index.ts
+++ packages/grammars/src/planner/parsing/index.ts
@@ -10,10 +10,45 @@ export interface SourcedSyntaxNode extends SyntaxNode {
   /**
    * The source code that this node was parsed from
    */
-  source: string;
+  readonly source: string;
 }
 
 export function parse(workoutPlan: string): SourcedSyntaxNode {
-  const result = parser.parse(workoutPlan).topNode;
-  // now what?
+  return bindNode(parser.parse(workoutPlan).topNode, workoutPlan);
 }
+
+function bindNode(node: SyntaxNode, source: string): SourcedSyntaxNode {
+  const recurse = (node: SyntaxNode | null) => bindMaybeNode(node, source);
+  const recurseSure = (node: SyntaxNode) => bindNode(node, source);
+  return {
+    ...node,
+    source: source,
+    childAfter: (...args) => recurse(node.childAfter(...args)),
+    childBefore: (...args) => recurse(node.childBefore(...args)),
+    enter: (...args) => recurse(node.enter(...args)),
+    get nextSibling() {
+      return recurse(node.nextSibling);
+    },
+    get prevSibling() {
+      return recurse(node.prevSibling);
+    },
+    get parent() {
+      return recurse(node.parent);
+    },
+    get firstChild() {
+      return recurse(node.firstChild);
+    },
+    get lastChild() {
+      return recurse(node.lastChild);
+    },
+    resolve: (...args) => recurseSure(node.resolve(...args)),
+    resolveInner: (...args) => recurseSure(node.resolveInner(...args)),
+    enterUnfinishedNodesBefore: (...args) =>
+      recurseSure(node.enterUnfinishedNodesBefore(...args)),
+    getChild: (...args) => recurse(node.getChild(...args)),
+    getChildren: (...args) => node.getChildren(...args).map(recurseSure),
+  };
+}
+
+const bindMaybeNode = (node: SyntaxNode | null, source: string) =>
+  node ? bindNode(node, source) : null;
