c20e2409b26a713c46bc58e51b209d6834d52882
AustinGrey <corey.crapo@gmail.com>
2026-06-05 17:34:56 -0600

    Make use of tools

--- packages/grammars/src/logic/evaluators/node-inc-assignment-expression.ts
+++ packages/grammars/src/logic/evaluators/node-inc-assignment-expression.ts
@@ -1,9 +1,12 @@
 import type { LogicHandler } from "@/logic/evaluators/types.ts";
 import { queryChildren } from "@/utils/grammars.ts";
 import { NodeName } from "@/evaluators/logic-evaluator.ts";
+import { is, isNumber } from "@/utils/types.ts";
+import { TDynamicWeight, TWeight } from "@/quantities/weight.ts";
+import type { IProgramState } from "@/common-types.ts";
 
 export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
-  const [stateVar, incAssignmentExpr, expression] = getChildren(expr);
+  const [stateVar, incAssignmentExpr, expression] = queryChildren(n);
   if (
     stateVar == null ||
     (stateVar.type.name !== NodeName.StateVariable &&
@@ -17,13 +20,13 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
   if (stateVar.type.name === NodeName.VariableExpression) {
     const nameNode = stateVar.getChild(NodeName.Keyword);
     if (nameNode == null) {
-      this.error(`Missing variable name`, stateVar);
+      t.error(`Missing variable name`, stateVar);
     }
     const indexExprs = stateVar.getChildren(NodeName.VariableIndex);
     const variable = this.getValue(nameNode);
     if (variable === "rm1") {
       if (indexExprs.length > 0) {
-        this.error(`rm1 is not an array`, expr);
+        t.error(`rm1 is not an array`, n);
       }
       const evaluatedValue = this.evaluate(expression);
       let value = Array.isArray(evaluatedValue)
@@ -58,10 +61,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
           this.unit,
         );
       } else {
-        this.error(
-          `Unknown operator ${op} after ${variable}`,
-          incAssignmentExpr,
-        );
+        t.error(`Unknown operator ${op} after ${variable}`, incAssignmentExpr);
       }
       return this.bindings.rm1;
     } else if (
@@ -83,10 +83,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
         op !== "*=" &&
         op !== "/="
       ) {
-        this.error(
-          `Unknown operator ${op} after ${variable}`,
-          incAssignmentExpr,
-        );
+        t.error(`Unknown operator ${op} after ${variable}`, incAssignmentExpr);
       }
       return this.recordVariableUpdate(variable, expression, indexExprs, op);
     } else if (this.mode === "update" && variable === "numberOfSets") {
@@ -98,10 +95,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
         op !== "*=" &&
         op !== "/="
       ) {
-        this.error(
-          `Unknown operator ${op} after ${variable}`,
-          incAssignmentExpr,
-        );
+        t.error(`Unknown operator ${op} after ${variable}`, incAssignmentExpr);
       }
       return this.changeNumberOfSets(expression, op);
     } else if (
@@ -120,21 +114,16 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
         op !== "*=" &&
         op !== "/="
       ) {
-        this.error(
-          `Unknown operator ${op} after ${variable}`,
-          incAssignmentExpr,
-        );
+        t.error(`Unknown operator ${op} after ${variable}`, incAssignmentExpr);
       }
       return this.changeBinding(variable, expression, indexExprs, op);
     } else {
-      this.error(`Unknown variable '${variable}'`, stateVar);
+      t.error(`Unknown variable '${variable}'`, stateVar);
     }
   } else if (stateVar.type.name === NodeName.Variable) {
     const varKey = this.getValue(stateVar).replace("var.", "");
     let value = this.evaluate(expression);
-    if (
-      !(Weight_is(value) || Weight_isPct(value) || typeof value === "number")
-    ) {
+    if (!(is(TWeight, value) || is(TDynamicWeight, value) || isNumber(value))) {
       value = value ? 1 : 0;
     }
     const op = this.getValue(incAssignmentExpr);
@@ -145,7 +134,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
       op !== "*=" &&
       op !== "/="
     ) {
-      this.error(`Unknown operator ${op} after ${varKey}`, incAssignmentExpr);
+      t.error(`Unknown operator ${op} after ${varKey}`, incAssignmentExpr);
     }
     const currentValue = this.vars[varKey];
     if (op === "+=") {
@@ -157,7 +146,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
     } else if (op === "/=") {
       this.vars[varKey] = this.divide(currentValue, value);
     } else {
-      this.error(`Unknown operator ${op} after ${varKey}`, incAssignmentExpr);
+      t.error(`Unknown operator ${op} after ${varKey}`, incAssignmentExpr);
     }
     return this.vars[varKey];
   } else {
@@ -170,7 +159,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
         if (stateKey in this.state) {
           state = this.state;
         } else {
-          this.error(`There's no state variable '${stateKey}'`, stateVar);
+          t.error(`There's no state variable '${stateKey}'`, stateVar);
         }
       } else {
         const indexEval = this.evaluate(indexNode);
@@ -181,11 +170,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
       let value = this.evaluate(expression);
       if (state != null) {
         if (
-          !(
-            Weight_is(value) ||
-            Weight_isPct(value) ||
-            typeof value === "number"
-          )
+          !(is(TWeight, value) || is(TDynamicWeight, value) || isNumber(value))
         ) {
           value = value ? 1 : 0;
         }
@@ -200,7 +185,7 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
         } else if (op === "/=") {
           state[stateKey] = this.divide(currentValue, value);
         } else {
-          this.error(
+          t.error(
             `Unknown operator ${op} after state.${stateKey}`,
             incAssignmentExpr,
           );
