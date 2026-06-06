e6d9e2450dd05d87c490c71e03d2ee15e5db9a74
AustinGrey <corey.crapo@gmail.com>
2026-06-06 08:43:39 -0600

    switch embed

--- packages/grammars/src/logic/evaluators/node-inc-assignment-expression.ts
+++ packages/grammars/src/logic/evaluators/node-inc-assignment-expression.ts
@@ -48,155 +48,173 @@ export const handler: LogicHandler<"IncAssignmentExpression"> = (n, t) => {
   /* prettier-ignore */ const multiply= <L extends Quantity | undefined, R extends Quantity | undefined>(l: L, r: R) => operate(l, r, (a, b) => a * b, (d, u)=>convertToWeight(t.getGlobal("rm1"), d, u), "*", (message)=>t.error(message, n));
   /* prettier-ignore */ const divide  = <L extends Quantity | undefined, R extends Quantity | undefined>(l: L, r: R) => operate(l, r, (a, b) => a / b, (d, u)=>convertToWeight(t.getGlobal("rm1"), d, u), "/", (message)=>t.error(message, n));
 
-  if (stateVar.type.name === NodeName.VariableExpression) {
-    const nameNode = queryChild(stateVar, { ofType: NodeName.Keyword });
-    if (nameNode == null) {
-      t.error(`Missing variable name`, stateVar);
-    }
-    const [...indexExprs] = queryChildren(stateVar, {
-      ofType: NodeName.VariableIndex,
-    });
-    const variable = t.getText(nameNode);
-    if (variable === "rm1") {
-      if (indexExprs.length > 0) {
-        t.error(`rm1 is not an array`, n);
+  switch (stateVar.type.name) {
+    case NodeName.StateVariable: {
+      const nameNode = queryChild(stateVar, { ofType: NodeName.Keyword });
+      if (nameNode == null) {
+        t.error(`Missing variable name`, stateVar);
       }
-      const value = coerceToQuantity(t.recurse(expression));
+      const [...indexExprs] = queryChildren(stateVar, {
+        ofType: NodeName.VariableIndex,
+      });
+      const variable = t.getText(nameNode);
+      if (variable === "rm1") {
+        if (indexExprs.length > 0) {
+          t.error(`rm1 is not an array`, n);
+        }
+        const value = coerceToQuantity(t.recurse(expression));
 
-      const op = t.getText(incAssignmentExpr);
-      t.updateGlobal("rm1", (rm1) =>
-        convertToWeight(
-          rm1,
-          op === "+="
-            ? add(rm1, value)
-            : op === "-="
-              ? subtract(rm1, value)
-              : op === "*="
-                ? multiply(rm1, value)
-                : op === "/="
-                  ? divide(rm1, value)
-                  : t.error(
-                      `Unknown operator ${op} after ${variable}`,
-                      incAssignmentExpr,
-                    ),
-          // @todo why use this.unit? When you can do all the math in kg and just adjust at display time?
-          // this.unit
-          "kg",
-        ),
-      );
-      return t.getGlobal("rm1");
-    } else if (
-      t.mode === "planner" &&
-      isOneOf(
-        variable,
-        "reps",
-        "weights",
-        "RPE",
-        "minReps",
-        "timers",
-        "setVariationIndex",
-        "descriptionIndex",
-        "numberOfSets",
-      )
-    ) {
-      const op = t.getText(incAssignmentExpr);
-      return isOneOf(op, "=", "+=", "-=", "*=", "/=")
-        ? recordVariableUpdate(variable, expression, indexExprs, op, t)
-        : t.error(
-            `Unknown operator ${op} after ${variable}`,
-            incAssignmentExpr,
-          );
-    } else if (t.mode === "update" && variable === "numberOfSets") {
-      const op = t.getText(incAssignmentExpr);
-      return isOneOf(op, "=", "+=", "-=", "*=", "/=")
-        ? changeNumberOfSets(expression, op, t)
-        : t.error(
-            `Unknown operator ${op} after ${variable}`,
-            incAssignmentExpr,
-          );
-    } else if (
-      t.mode === "update" &&
-      isOneOf(variable, "reps", "weights", "RPE", "minReps", "timers")
-    ) {
-      const op = t.getText(incAssignmentExpr);
-      return isOneOf(op, "=", "+=", "-=", "*=", "/=")
-        ? changeBinding(variable, expression, indexExprs, op, t)
-        : t.error(
-            `Unknown operator ${op} after ${variable}`,
-            incAssignmentExpr,
-          );
-    } else {
-      return t.error(`Unknown variable '${variable}'`, stateVar);
-    }
-  } else if (stateVar.type.name === NodeName.Variable) {
-    const varKey = t.getText(stateVar).replace("var.", "");
-    let value = t.recurse(expression);
-    if (!(is(TWeight, value) || is(TDynamicWeight, value) || isNumber(value))) {
-      value = value ? 1 : 0;
-    }
-    const op = t.getText(incAssignmentExpr);
-    if (isOneOf(op, "=", "+=", "-=", "*=", "/=")) {
-      switch (op) {
-        case "+=":
-          return t.updateVar(varKey, add(t.getVar(varKey), value));
-        case "-=":
-          return t.updateVar(varKey, subtract(t.getVar(varKey), value));
-        case "*=":
-          return t.updateVar(varKey, multiply(t.getVar(varKey), value));
-        case "/=":
-          return t.updateVar(varKey, divide(t.getVar(varKey), value));
-        default:
-          op satisfies never;
-          t.error(`Unknown operator ${op} after ${varKey}`, incAssignmentExpr);
-      }
-    } else {
-      t.error(`Unknown operator ${op} after ${varKey}`, incAssignmentExpr);
-    }
-  } else {
-    const indexNode = queryChild(stateVar, {
-      ofType: NodeName.StateVariableIndex,
-    });
-    const stateKeyNode = queryChild(stateVar, { ofType: NodeName.Keyword });
-    if (stateKeyNode == null) {
-      return 0;
-    }
-    const stateKey = t.getText(stateKeyNode);
-    let state: IProgramState | undefined;
-    if (indexNode == null) {
-      if (stateKey in this.state) {
-        state = this.state;
+        const op = t.getText(incAssignmentExpr);
+        t.updateGlobal("rm1", (rm1) =>
+          convertToWeight(
+            rm1,
+            op === "+="
+              ? add(rm1, value)
+              : op === "-="
+                ? subtract(rm1, value)
+                : op === "*="
+                  ? multiply(rm1, value)
+                  : op === "/="
+                    ? divide(rm1, value)
+                    : t.error(
+                        `Unknown operator ${op} after ${variable}`,
+                        incAssignmentExpr,
+                      ),
+            // @todo why use this.unit? When you can do all the math in kg and just adjust at display time?
+            // this.unit
+            "kg",
+          ),
+        );
+        return t.getGlobal("rm1");
+      } else if (
+        t.mode === "planner" &&
+        isOneOf(
+          variable,
+          "reps",
+          "weights",
+          "RPE",
+          "minReps",
+          "timers",
+          "setVariationIndex",
+          "descriptionIndex",
+          "numberOfSets",
+        )
+      ) {
+        const op = t.getText(incAssignmentExpr);
+        return isOneOf(op, "=", "+=", "-=", "*=", "/=")
+          ? recordVariableUpdate(variable, expression, indexExprs, op, t)
+          : t.error(
+              `Unknown operator ${op} after ${variable}`,
+              incAssignmentExpr,
+            );
+      } else if (t.mode === "update" && variable === "numberOfSets") {
+        const op = t.getText(incAssignmentExpr);
+        return isOneOf(op, "=", "+=", "-=", "*=", "/=")
+          ? changeNumberOfSets(expression, op, t)
+          : t.error(
+              `Unknown operator ${op} after ${variable}`,
+              incAssignmentExpr,
+            );
+      } else if (
+        t.mode === "update" &&
+        isOneOf(variable, "reps", "weights", "RPE", "minReps", "timers")
+      ) {
+        const op = t.getText(incAssignmentExpr);
+        return isOneOf(op, "=", "+=", "-=", "*=", "/=")
+          ? changeBinding(variable, expression, indexExprs, op, t)
+          : t.error(
+              `Unknown operator ${op} after ${variable}`,
+              incAssignmentExpr,
+            );
       } else {
-        t.error(`There's no state variable '${stateKey}'`, stateVar);
+        return t.error(`Unknown variable '${variable}'`, stateVar);
       }
-    } else {
-      const indexEval = t.recurse(indexNode);
-      const index = toNumberUnsafe(indexEval);
-      state = this.otherStates[index];
     }
+    case NodeName.VariableExpression: {
+      const varKey = t.getText(stateVar).replace("var.", "");
+      let value = t.recurse(expression);
+      if (
+        !(is(TWeight, value) || is(TDynamicWeight, value) || isNumber(value))
+      ) {
+        value = value ? 1 : 0;
+      }
+      const op = t.getText(incAssignmentExpr);
+      if (isOneOf(op, "=", "+=", "-=", "*=", "/=")) {
+        switch (op) {
+          case "+=":
+            return t.updateVar(varKey, add(t.getVar(varKey), value));
+          case "-=":
+            return t.updateVar(varKey, subtract(t.getVar(varKey), value));
+          case "*=":
+            return t.updateVar(varKey, multiply(t.getVar(varKey), value));
+          case "/=":
+            return t.updateVar(varKey, divide(t.getVar(varKey), value));
+          case "=":
+            // @todo this would be solved if we used the desugaring method - this would never be reached
+            return t.error(
+              `Unknown operator ${op} after ${varKey}`,
+              incAssignmentExpr,
+            );
+          default:
+            return op satisfies never;
+        }
+      } else {
+        return t.error(
+          `Unknown operator ${op} after ${varKey}`,
+          incAssignmentExpr,
+        );
+      }
+    }
+    case NodeName.Variable: {
+      const indexNode = queryChild(stateVar, {
+        ofType: NodeName.StateVariableIndex,
+      });
+      const stateKeyNode = queryChild(stateVar, { ofType: NodeName.Keyword });
+      if (stateKeyNode == null) {
+        return 0;
+      }
+      const stateKey = t.getText(stateKeyNode);
+      let state: IProgramState | undefined;
+      if (indexNode == null) {
+        if (stateKey in this.state) {
+          state = this.state;
+        } else {
+          t.error(`There's no state variable '${stateKey}'`, stateVar);
+        }
+      } else {
+        const indexEval = t.recurse(indexNode);
+        const index = toNumberUnsafe(indexEval);
+        state = this.otherStates[index];
+      }
 
-    let value = t.recurse(expression);
-    if (state == null) {
-      return value;
+      let value = t.recurse(expression);
+      if (state == null) {
+        return value;
+      }
+      if (
+        !(is(TWeight, value) || is(TDynamicWeight, value) || isNumber(value))
+      ) {
+        value = value ? 1 : 0;
+      }
+      const op = t.getText(incAssignmentExpr);
+      const currentValue = state[stateKey] ?? 0;
+      if (op === "+=") {
+        state[stateKey] = add(currentValue, value);
+      } else if (op === "-=") {
+        state[stateKey] = subtract(currentValue, value);
+      } else if (op === "*=") {
+        state[stateKey] = multiply(currentValue, value);
+      } else if (op === "/=") {
+        state[stateKey] = divide(currentValue, value);
+      } else {
+        t.error(
+          `Unknown operator ${op} after state.${stateKey}`,
+          incAssignmentExpr,
+        );
+      }
+      return state[stateKey];
     }
-    if (!(is(TWeight, value) || is(TDynamicWeight, value) || isNumber(value))) {
-      value = value ? 1 : 0;
-    }
-    const op = t.getText(incAssignmentExpr);
-    const currentValue = state[stateKey] ?? 0;
-    if (op === "+=") {
-      state[stateKey] = add(currentValue, value);
-    } else if (op === "-=") {
-      state[stateKey] = subtract(currentValue, value);
-    } else if (op === "*=") {
-      state[stateKey] = multiply(currentValue, value);
-    } else if (op === "/=") {
-      state[stateKey] = divide(currentValue, value);
-    } else {
-      t.error(
-        `Unknown operator ${op} after state.${stateKey}`,
-        incAssignmentExpr,
-      );
-    }
-    return state[stateKey];
+    default:
+      stateVar.type.name satisfies never;
   }
 };
