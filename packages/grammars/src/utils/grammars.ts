ed7cf3188afc38d4bc32e5a05bfaa78ca9eca418
AustinGrey <corey.crapo@gmail.com>
2026-06-06 19:54:25 -0600

    drop descendant methods since they might be broken and are unused

--- packages/grammars/src/utils/grammars.ts
+++ packages/grammars/src/utils/grammars.ts
@@ -71,63 +71,6 @@ export function getChild<TTypes extends string>(
 }
 
 /**
- * @todo THIS MIGHT BE BROKEN AND NOT DO WHAT YOU EXPECT. CURSORS ARE WEIRD!!!!!!!
- * @yields all descendants of a syntax node in depth-first (pre-order) order.
- * @param node The node to get descendants of
- * @param options
- * @param options.atLeast - If provided, throws an error if fewer than this many matching descendants are found
- * @param options.ofType - If provided, only yields descendants of this type
- * @param options.includeSkipped - If true, includes skipped nodes in the result. Otherwise they are skipped.
- */
-export function* queryDescendants<TTypes extends string>(
-  node: SourcedSyntaxNode,
-  { atLeast, ofType, includeSkipped }: QueryOptions<TTypes> = {},
-): Generator<SourcedSyntaxNode> {
-  const cur = node.cursor();
-  let count = 0;
-  // First .next ensures we skip the node itself
-  while (cur.next()) {
-    const current = cur.node;
-    const matchesType = !ofType || current.type.name === ofType;
-    const matchesSkipped = includeSkipped || !current.type.isSkipped;
-    if (matchesType && matchesSkipped) {
-      yield current;
-      count++;
-    }
-  }
-  if (atLeast !== undefined && atLeast !== count) {
-    throw new SyntaxError(
-      `Expected at least ${atLeast} descendant${atLeast === 1 ? "" : "s"}${ofType ? ` of type ${ofType}` : ""}, but got ${count}`,
-    );
-  }
-}
-
-/**
- * @todo THIS MIGHT BE BROKEN AND NOT DO WHAT YOU EXPECT. CURSORS ARE WEIRD!!!!!!!
- * Gets the descendant of a node that matches the given type.
- * Throws if there is more than one matching descendant.
- * @param node The node to get the first matching descendant of
- * @param options The query options to use when searching for the descendant
- */
-export function getDescendant<TTypes extends string>(
-  node: SourcedSyntaxNode,
-  options: QueryOptions<TTypes> = {},
-): SourcedSyntaxNode {
-  const [result, ...rest] = queryDescendants(node, options);
-  if (!result) {
-    throw new SyntaxError(
-      `Expected descendant${options.ofType ? ` of type ${options.ofType}` : ""}, but found none`,
-    );
-  }
-  if (rest.length > 0) {
-    throw new SyntaxError(
-      `Expected only one descendant${options.ofType ? ` of type ${options.ofType}` : ""}, but found ${rest.length}`,
-    );
-  }
-  return result;
-}
-
-/**
  * Gets child, or returns undefined if there are no children
  * @param node The node to get the first matching child of
  * @param options Additional options to pass along to queryChildren
