a8c68c0e0ce1671c7c0362178bfe2540bb34957c
AustinGrey <corey.crapo@gmail.com>
2026-06-11 00:09:16 -0600

    Simplify, validate types, don't parse values

--- packages/grammars/src/utils/collection.ts
+++ packages/grammars/src/utils/collection.ts
@@ -1,18 +1,19 @@
+import type { ConditionalPick } from "type-fest";
+
 export const CollectionUtils_compact = <T>(arr: (T | undefined)[]): T[] =>
   arr.filter((i) => i !== undefined);
 
-export function CollectionUtils_sortBy<T extends {}, K extends keyof T>(
-  arr: T[],
-  key: K,
-  isReverse?: boolean,
-): T[K] extends number ? T[] : never {
-  const arrCopy = [...arr];
-  arrCopy.sort((a, b) => {
-    const aVal = a[key] as any;
-
-    const bVal = b[key] as any;
-    return isReverse ? bVal - aVal : aVal - bVal;
-  });
-
-  return arrCopy as any;
+/**
+ * Sorts an array of objects by the given numeric key.
+ * @param arr The array to sort.
+ * @param key The key to sort by.
+ * @param isReverse Whether to sort in reverse order.
+ */
+export function CollectionUtils_sortBy<
+  T extends {},
+  K extends keyof ConditionalPick<T, number> & keyof T,
+>(arr: T[], key: K, isReverse?: boolean): T[] {
+  return arr.toSorted(
+    (a, b) => (Number(a[key]) - Number(b[key])) * (isReverse ? -1 : 1),
+  );
 }
