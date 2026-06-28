39cd36d8415a0660b39078d800fb68a72ebbff7c
Austin <corey.crapo@gmail.com>
2026-06-28 09:12:39 -0600

    Revert "Push in the try/catch so I can unwrap it at all call sites"
    This reverts commit b0a78435d3244c0fcecca41b1522e01181f8ff73.

--- packages/grammars/src/evaluators/plan-evaluator-minimal.ts
+++ packages/grammars/src/evaluators/plan-evaluator-minimal.ts
@@ -292,28 +292,34 @@ function Program_runFinishDayScript(
   );
 
   const otherStates = structuredClone(program.states);
-  // @todo replace all syntaxerror throws with returns, and unwrap this try/catch. Confirmed that only run itself throws, not the functions called to make the args.
-  const result = run(
-    PlannerProgramExercise_getProgressScript(programExercise) || "",
-    {
-      ...state,
-      ...userPromptedStateVars,
-    },
-    bindings,
-    Progress_createScriptFunctions(settings),
-    {
-      exerciseType: programExercise.exerciseType,
-      unit: settings.units,
-      prints: [],
-    },
-    otherStates,
-    IProgramMode.PLANNER,
-  );
+  let updates: ILiftoscriptEvaluatorUpdate[];
+  let newState: IProgramState;
+  try {
+    const result = run(
+      PlannerProgramExercise_getProgressScript(programExercise) || "",
+      {
+        ...state,
+        ...userPromptedStateVars,
+      },
+      bindings,
+      Progress_createScriptFunctions(settings),
+      {
+        exerciseType: programExercise.exerciseType,
+        unit: settings.units,
+        prints: [],
+      },
+      otherStates,
+      IProgramMode.PLANNER,
+    );
+    updates = result.updates;
+    newState = result.finalState;
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      return { success: false, error: e.message };
+    }
+    throw e;
+  }
 
-  const stateDiff = omitBy(
-    result.finalState,
-    (value, key) => state[key] === value,
-  );
   const diffOtherStates = ObjectUtils_keys(otherStates).reduce<
     IByTag<IProgramState>
   >((memo, key) => {
@@ -331,14 +337,10 @@ function Program_runFinishDayScript(
     return memo;
   }, {});
 
+  const stateDiff = omitBy(newState, (value, key) => state[key] === value);
   return {
     success: true,
-    data: {
-      state: stateDiff,
-      otherStates: diffOtherStates,
-      updates: result.updates,
-      bindings,
-    },
+    data: { state: stateDiff, otherStates: diffOtherStates, updates, bindings },
   };
 }
 
@@ -1806,26 +1808,33 @@ function Progress_runUpdateScriptForEntry(
     as1(setVariationIndex),
     as1(findIndexOfCurrentOrFirst(programExercise.descriptions.values)),
   );
-  const fnContext: IScriptFnContext = {
-    exerciseType: exercise,
-    unit: settings.units,
-    prints: [],
-  };
-  const { finalState } = run(
-    script,
-    state,
-    bindings,
-    Progress_createScriptFunctions(settings),
-    fnContext,
-    structuredClone(otherStates),
-    IProgramMode.UPDATE,
-  );
-  const newEntry = Progress_applyBindings(entry, bindings, settings);
-  newEntry.state = { ...newEntry.state, ...finalState };
-  if (fnContext.prints.length > 0) {
-    newEntry.updatePrints = fnContext.prints;
+  try {
+    const fnContext: IScriptFnContext = {
+      exerciseType: exercise,
+      unit: settings.units,
+      prints: [],
+    };
+    const result = run(
+      script,
+      state,
+      bindings,
+      Progress_createScriptFunctions(settings),
+      fnContext,
+      structuredClone(otherStates),
+      IProgramMode.UPDATE,
+    );
+    const newEntry = Progress_applyBindings(entry, bindings, settings);
+    newEntry.state = { ...newEntry.state, ...result.finalState };
+    if (fnContext.prints.length > 0) {
+      newEntry.updatePrints = fnContext.prints;
+    }
+    return newEntry;
+  } catch (error) {
+    const e = error as Error;
+    console.error(e);
+
+    return entry;
   }
-  return newEntry;
 }
 
 function Progress_applyBindings(
--- packages/grammars/src/logic/evaluators/index.ts
+++ packages/grammars/src/logic/evaluators/index.ts
@@ -25,8 +25,6 @@ import {
   type SourcedSyntaxNode,
 } from "@/utils/lezer.ts";
 import { queryTree } from "@/utils/grammars.ts";
-import type { IEither } from "@/utils/types.ts";
-import { LiftoscriptSyntaxError } from "@/evaluators/logic-evaluator.ts";
 
 /**
  * The handler for when we haven't decided how to handle a node
@@ -123,12 +121,6 @@ export function* validate(
  *   sites. But it doesn't seem necessary to do that since all math can be done in any unit, and then converted after
  *   to display in whichever unit is needed. So I'm leaving that off and will delete this comment once I'm sure it's not needed.
  * @param logic The script to run
- * @param initialState
- * @param globalData
- * @param publicFunctions
- * @param fnContext
- * @param otherStates
- * @param mode
  */
 export function run(
   logic: string,
@@ -140,7 +132,7 @@ export function run(
   otherStates: Record<string | number, IProgramState>,
   mode: IProgramMode,
 ): {
-  result: IEither<LogicResult, LiftoscriptSyntaxError>;
+  result: LogicResult;
   finalState: IProgramState;
   updates: ILiftoscriptEvaluatorUpdate[];
 } {
@@ -212,23 +204,8 @@ export function run(
     fnContext,
   };
 
-  // @todo this should keep getting pushed down so you can collect ALL errors before returning, rather than returning only the first error?
-  let result: IEither<LogicResult, LiftoscriptSyntaxError>;
-  try {
-    result = {
-      success: true,
-      data: handleLogic(parseBound(parser, logic), tools),
-    };
-  } catch (e) {
-    if (e instanceof LiftoscriptSyntaxError) {
-      result = { success: false, error: e };
-    } else {
-      throw e;
-    }
-  }
-
   return {
-    result,
+    result: handleLogic(parseBound(parser, logic), tools),
     finalState: state,
     updates,
   };
