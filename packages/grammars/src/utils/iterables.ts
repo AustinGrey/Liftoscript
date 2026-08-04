60a82ecd38ae8389a7754f3cfae1bff062dfe375
AustinGrey <corey.crapo@gmail.com>
2026-08-03 18:00:32 -0600

    Create a theoretical nested for unwrapper for simplification

--- packages/grammars/src/utils/iterables.ts
+++ packages/grammars/src/utils/iterables.ts
@@ -26,3 +26,17 @@ export function* generateRange<T>(
 		yield generator(i);
 	}
 }
+
+/**
+ * When traversing deeply nested elements, this simplifies the many for loop boiler plates into a single
+ * less nested call. Generating the end result elements, with the ancestor loop information.
+ * @param outer The top level iterable
+ * @param unwrappers The series of unwrappers to get from the top level to the leaf elements
+ * @returns the leaf element, and the context it's in. You'll generally want to destructure the context to get just the items you need
+ *    e.g. const [week,, day,, exercise, exerciseIndex] = context;
+ */
+export function nestedFor<A, B>(
+	outer: A[],
+	unwrappers: [(a: A) => B],
+): Generator<{ item: B; context: [A, IndexFrom0] }>;
+export function* nestedFor(outer: any[], unwrappers: ((u: any) => any)[]): Generator<any> {}
